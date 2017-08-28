73fc38df67ad03c7abc2e6420120b68a1cc541b7
kevin.hu <kevin.hu@kevinhude-iMac.local>
2017-08-28 18:29:55 +0800

    vue google map

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view></router-view> -->
+    <div class="map-container">
+      <i-map
+        :lat="-34.4"
+        :lng="150.644">
+      </i-map>
+    </div>
+  </div>
+</template>
+
+<script>
+import IMap from './components/IMap'
+export default {
+  name: 'app',
+
+  components: {
+    IMap
+  }
+}
+</script>
+
+<style lang="stylus">
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+}
+.map-container {
+  width: 800px;
+  height: 600px;
+  position: relative;
+}
+</style>
---
+++ src/stories/index.js
@@ -0,0 +1,124 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+/* eslint-disable */
+
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import IMap from '../components/IMap.vue';
+
+const lat = 23.90
+const lng = 121.07
+
+storiesOf('IMap', module)
+  .add('default', () => ({
+    components: { IMap },
+    template: `
+      <div style="width: 800px;height: 600px;">
+        <i-map
+          :lat="${lat}"
+          :lng="${lng}">
+        </i-map>
+      </div>`
+  }))
+  .add('with draggable marker', () => ({
+    components: { IMap },
+    template: `
+      <div style="width: 800px;height: 600px;">
+        <i-map
+          :lat="${lat}"
+          :lng="${lng}"
+          :draggable="true">
+        </i-map>
+      </div>`
+  }))
+  .add('with searchable input', () => ({
+    components: { IMap },
+    template: `
+      <div style="width: 800px;height: 600px;">
+        <i-map
+          :lat="${lat}"
+          :lng="${lng}"
+          :searchable="true">
+        </i-map>
+      </div>`
+  }))
+  .add('control latlng to specific decimal point (5)', () => ({
+    components: { IMap },
+    template: `
+      <div style="width: 800px;height: 600px;">
+        <i-map
+          :lat="${lat}"
+          :lng="${lng}"
+          :searchable="true"
+          :showDecimalPoint="5">
+        </i-map>
+      </div>`
+  }))
+  .add('ui control setting', () => ({
+    components: { IMap },
+    data () {
+      return {
+        controls: {
+          hasMapTypeControl: true,
+          hasScaleControl: true,
+          hasStreetViewControl: true,
+          hasRotateControl: true,
+          hasFullscreenControl: true,
+          hasZoomControl: true
+        },
+        shouldShow: true
+      }
+    },
+    template: `
+      <div style="width: 800px;height: 600px;">
+        <div style="text-align: center">
+          <label>
+            show map control
+            <input type="checkbox" v-model="controls.hasMapTypeControl" @change="refresh">
+          </label>
+          <label>
+            show street view control
+            <input type="checkbox" v-model="controls.hasStreetViewControl" @change="refresh">
+          </label>
+          <label>
+            show full screen control
+            <input type="checkbox" v-model="controls.hasFullscreenControl" @change="refresh">
+          </label>
+          <label>
+            show zoom control
+            <input type="checkbox" v-model="controls.hasZoomControl" @change="refresh">
+          </label>
+        </div>
+        <div style="text-align: center">
+          <label>
+            use rotate control
+            <input type="checkbox" v-model="controls.hasRotateControl" @change="refresh">
+          </label>
+          <label>
+            use sacle control
+            <input type="checkbox" v-model="controls.hasScaleControl" @change="refresh">
+          </label>
+        </div>
+        <i-map
+          v-if="shouldShow"
+          :lat="${lat}"
+          :lng="${lng}"
+          :searchable="true"
+          :hasMapTypeControl="controls.hasMapTypeControl"
+          :hasScaleControl="controls.hasScaleControl"
+          :hasStreetViewControl="controls.hasStreetViewControl"
+          :hasRotateControl="controls.hasRotateControl"
+          :hasFullscreenControl="controls.hasFullscreenControl"
+          :hasZoomControl="controls.hasZoomControl">
+        </i-map>
+      </div>`,
+    methods: {
+      refresh() {
+        this.shouldShow = false
+        setTimeout(() => {
+          this.shouldShow = true
+        }, 100)
+      }
+    }
+  }))
