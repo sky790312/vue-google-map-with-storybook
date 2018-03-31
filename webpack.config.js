843f4f228e1307ab2dbbe05c217d89a82d7d9865
kevin.hu <kevin.hu@kevinhude-iMac.local>
2018-03-31 16:49:42 +0800

    update setting

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   //   filename: 'build.js'
   // },
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, './dist'),
     publicPath: '',
     filename: 'vue-google-map.js',
     library: 'VueGoogleMap',
